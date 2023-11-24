2a464b6268180e03352b8e55bd0acd6a5c24fafc
Aleksandre Chakhvashvili <aleksandre.chakhvashvili24@gmail.com>
2023-11-24 23:58:14 +0400

    polish header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -12,12 +12,16 @@ export default function Header() {
     const [isSticky, setIsSticky] = useState(false);
 
     const navigation = [
-        { name: "HOME", href: "/" },
-        { name: "ABOUT", href: "/about" },
-        { name: "ROOMS", href: "/rooms" },
-        { name: "GALLERY", href: "/gallery" },
-        { name: "CONTACT", href: "/contact" },
+        { name: "Home", href: "/" },
+        { name: "About", href: "/about" },
+        { name: "Rooms", href: "/rooms" },
+        { name: "Gallery", href: "/gallery" },
+        { name: "Conctact", href: "/contact" },
     ];
+
+    const handleNav = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
 
     useEffect(() => {
         setActivePage(usePathname);
@@ -35,89 +39,90 @@ export default function Header() {
         };
     }, []);
 
+    useEffect(() => {
+        if (isMobileMenuOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "";
+        }
+    }, [isMobileMenuOpen]);
+
     return (
         <header
-            className={`${isSticky ? "" : "bg-opacity-0"} ${
-                activePage === "/" ? "fixed" : "sticky bg-[#191919]"
-            }  mt-[6rem] inset-x-0 top-0 z-50 transition-colors duration-300  px-4 `}
+            className={`${
+                isSticky ? "fixed top-0 bg-[#191919]" : "absolute top-24"
+            } z-50 w-full h-24 transition-colors duration-300`}
         >
-            <div className="container mx-auto flex justify-around items-center">
-                <div className="flex items-center space-x-2">
-                    <Link href={"/"} onClick={() => setActivePage("/")}>
-                        <Image
-                            src="/img/pngegg.png"
-                            alt="Hotel Logo"
-                            width={150}
-                            height={50}
-                        />
-                    </Link>
-                </div>
-                <div className="md:hidden">
-                    <button
-                        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-                        className="text-white z-50"
-                    >
-                        {isMobileMenuOpen ? (
-                            <Bars3Icon
-                                className="h-6 w-6 hidden"
-                                aria-hidden="true"
-                            />
-                        ) : (
-                            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                        )}
-                    </button>
-                </div>
-                <nav
-                    className={`${
-                        isMobileMenuOpen ? "hidden" : ""
-                    } md:flex space-x-6 text-xl hidden`}
-                >
-                    {navigation.map((item) => (
-                        <Link
-                            key={item.name}
-                            href={item.href}
-                            className={`transition duration-200 ease-in-out hover:text-orange-500 
+            <div className="container flex justify-between items-center h-full mx-auto px-4 sm:px-6 lg:px-8 xl:px-12 2xl:px-16">
+                <Link href={"/"} onClick={() => setActivePage("/")}>
+                    <Image
+                        src="/img/pngegg.png"
+                        alt="Hotel Logo"
+                        width={150}
+                        height={50}
+                    />
+                </Link>
+                <nav className="hidden md:flex">
+                    <ul className="hidden md:flex">
+                        {navigation.map((item) => (
+                            <Link key={item.name} href={item.href}>
+                                <li
+                                    className={`ml-10 uppercase text-xl transition duration-200 ease-in-out hover:text-orange-500 
                             ${
                                 activePage === item.href
                                     ? "text-orange-500"
                                     : "text-white"
                             }`}
-                            onClick={() => setActivePage(item.href)}
-                        >
-                            {item.name}
-                        </Link>
-                    ))}
+                                    onClick={() => setActivePage(item.href)}
+                                >
+                                    {item.name}
+                                </li>
+                            </Link>
+                        ))}
+                    </ul>
                 </nav>
+                <div
+                    onClick={handleNav}
+                    className="md:hidden cursor-pointer pl-24"
+                >
+                    <Bars3Icon
+                        className="text-white h-9 w-9"
+                        aria-hidden="true"
+                    />
+                </div>
             </div>
             <div
                 className={`${
-                    isMobileMenuOpen ? "block" : "hidden"
-                } md:hidden fixed inset-0 bg-black bg-opacity-75`}
+                    isMobileMenuOpen ? "translate-x-0" : "-translate-x-full"
+                } fixed top-0 left-0 w-full h-screen bg-black ease-in-out duration-300 md:hidden`}
             >
-                <button
-                    onClick={() => setIsMobileMenuOpen(false)}
-                    className="text-white absolute top-4 right-4 z-50"
+                <div
+                    onClick={handleNav}
+                    className="cursor-pointer text-white absolute top-5 right-5"
                 >
-                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-                <nav className="h-full flex flex-col items-center justify-center space-y-4">
-                    {navigation.map((item) => (
-                        <Link
-                            key={item.name}
-                            href={item.href}
-                            className={`text-lg p-2 rounded-lg ${
-                                activePage === item.name
-                                    ? "text-orange-500"
-                                    : "text-white"
-                            }`}
-                            onClick={() => {
-                                setActivePage(item.name);
-                                setIsMobileMenuOpen(false);
-                            }}
-                        >
-                            {item.name}
-                        </Link>
-                    ))}
+                    <XMarkIcon className="h-9 w-9" aria-hidden="true" />
+                </div>
+                <nav className="flex flex-col justify-center items-center h-full w-full">
+                    <ul className="flex flex-col items-center gap-6">
+                        {navigation.map((item) => (
+                            <Link key={item.name} href={item.href}>
+                                <li
+                                    className={`uppercase w-40 text-center text-xl py-2 border-b-2 border-transparent transition duration-200 ease-in-out hover:text-orange-500 hover:border-orange-500
+                        ${
+                            activePage === item.href
+                                ? "text-orange-500"
+                                : "text-white"
+                        }`}
+                                    onClick={() => {
+                                        setActivePage(item.href);
+                                        setIsMobileMenuOpen(false);
+                                    }}
+                                >
+                                    {item.name}
+                                </li>
+                            </Link>
+                        ))}
+                    </ul>
                 </nav>
             </div>
         </header>
